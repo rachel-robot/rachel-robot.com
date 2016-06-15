f49c6d1663ce8101c53b519d43016566175f19ce
Rachel Luczkowski <rachel.luczkowski@gmail.com>
2016-06-14 22:30:14 -0400

    Tue Jun 14 22:30:14 EDT 2016

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PUBDIR=$DIR/../public
 cd "$DIR/.."
 bash $DIR/save.sh
-hugo
+hugo || { echo 'Error making the pages, read the text!' ; sleep 30; exit 1; }
 scp -r $PUBDIR/* rachel:/home/rachel/rachel-robot.com
