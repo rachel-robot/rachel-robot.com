70b61ed4de611b67b5ce621d32265b37c66a6901
Rachel Luczkowski <rachel.luczkowski@gmail.com>
2016-06-14 22:25:52 -0400

    Tue Jun 14 22:25:52 EDT 2016

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-hugo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PUBDIR=$DIR/../public
+cd "$DIR/.."
+hugo
 scp -r $PUBDIR/* rachel:/home/rachel/rachel-robot.com
--- scripts/save.sh
+++ scripts/save.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PUBDIR=$DIR/../public
+cd "$DIR/.."
 git ci -am "`date`" && git push
